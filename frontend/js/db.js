97d9c26f204aed3fefdf71ea574fc4ac3d0b8f5b
Alaa Eldin ABDALLA <ibnkutomi@gmail.com>
2026-04-02 12:17:57 +0200

    # To-Do (aus Feedback) - [x] Mindest-Schriftgröße: 16px - [x] Ordnerstruktur angepasst - [x] Contentbegrenzung auf 1440px

--- frontend/js/db.js
+++ frontend/js/db.js
@@ -1,6 +1,75 @@
+
+let dishes = [
+  {
+    img: "./assets/images/dish-1.jpg",
+    name: "Gebratener Lachs",
+    price: 17.5,
+    description: "Lachsfilet mit Kartoffeln.",
+  },
+
+  {
+    img: "./assets/images/dish-2.jpg",
+    name: "Spaghetti Carbonara",
+    price: 12.5,
+    description: "Klassische italienische Pasta.",
+  },
+  {
+    img: "./assets/images/dish-3.jpg",
+    name: "Thai Curry mit Reis",
+    price: 13.5,
+    description: "Aromatisches Curry mit Gemüse.",
+  },
+  {
+    img: "./assets/images/dish-4.jpg",
+    name: "Gegrilltes Hähnchen",
+    price: 15.5,
+    description: "Saftiges Hähnchenfilet mit Gemüse.",
+  },
+
+  {
+    img: "./assets/images/dish-5.jpg",
+    name: "Vegetarische Lasagne",
+    price: 10.5,
+    description: "Leckere Lasagne mit Gemüse und Käse.",
+  },
+  {
+    img: "./assets/images/dish-6.jpg",
+    name: "Hähnchen-Curry mit Gemüse",
+    price: 14.5,
+    description: "Würziges Curry mit Hähnchen und Gemüse.",
+  },
+];
+
+/* Gerichte */
+function renderDishes() {
+  const dishContainerRef = document.getElementById("dishContainer");
+
+  dishContainerRef.innerHTML = "";
+
+  for (let dishIndex = 0; dishIndex < dishes.length; dishIndex++) {
+    const selectedDish = dishes[dishIndex];
+    dishContainerRef.innerHTML += dishTemplate(selectedDish, dishIndex);
+  }
+  
+}
+
+/*  Gericht in den Warenkorb*/
+function addDishToBasket(dishIndex) {
+  const selectedDish = dishes[dishIndex];
+  const existingItem = findBasketItem(selectedDish);
+
+  if (existingItem) { existingItem.amount++;
+  } else {
+    addNewItemToBasket(selectedDish);
+  }
+   
+  renderBasket();
+}
+/*  Warenkorb speichern */
 function saveBasket() {
   localStorage.setItem("basket", JSON.stringify(basket));
 }
+/*  Warenkorb laden */
 
 function loadBasket() {
   const savedBasket = localStorage.getItem("basket");
